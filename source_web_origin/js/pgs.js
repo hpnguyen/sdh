6fad64ca4eca7d4797784488344d537eb947aee7
Ha Phu Nguyen <hpn888@gmail.com>
2014-04-22 23:54:13 -0400

    Update JS file

--- source_web_origin/js/pgs.js
+++ source_web_origin/js/pgs.js
@@ -41,10 +41,30 @@ function changeHashAgain() {
   window.location.href += "B";
 }
 
-var storedHash = window.location.hash;
+/* var storedHash = window.location.hash;
 window.setInterval(function () {
     if (window.location.hash != storedHash) {
          window.location.hash = storedHash;
     }
-}, 50);
+}, 50); */
+
+function detectIE() {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf('MSIE ');
+    var trident = ua.indexOf('Trident/');
+
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    if (trident > 0) {
+        // IE 11 (or newer) => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    // other browser
+    return false;
+}
 // ---------------------
