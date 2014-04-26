1ca080320396f90b498ec7b4ba35ff139dc302a8
Ha Phu Nguyen <hpn888@gmail.com>
2014-04-26 05:38:32 -0400

    - Change Ip from 172.28.40.250 to 127.0.0.1

--- source_web_origin/gv/libs/connectdbweb.php
+++ source_web_origin/gv/libs/connectdbweb.php
@@ -1,4 +1,6 @@
 ﻿<?php
-$db = "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 172.28.40.250)(PORT = 1521)))(CONNECT_DATA=(SID=SDHbk)))";
+//$db_config_real_ip = "172.28.40.250";
+$db_config_real_ip = "127.0.0.1";
+$db = "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = ".$db_config_real_ip.")(PORT = 1521)))(CONNECT_DATA=(SID=SDHbk)))";
 $db_conn_hv = OCILogon("webweb", "weB210810",$db);
 ?>
