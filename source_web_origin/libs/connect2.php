882817a5e79364720da9e073fbebc7b0460c00ff
Ha Phu Nguyen <hpn888@gmail.com>
2014-05-05 22:47:24 -0400

    Change database password

--- source_web_origin/libs/connect2.php
+++ source_web_origin/libs/connect2.php
@@ -1,4 +1,4 @@
 ﻿<?php
 $db = "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 172.28.40.168)(PORT = 1521)))(CONNECT_DATA=(SID=SDHbk)))";
-$db_conn_gv = oci_connect("csdl", "lnminh",$db);
+$db_conn_gv = oci_connect("csdl", "LNMINH",$db);
 ?>
