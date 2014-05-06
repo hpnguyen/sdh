72c46ce05f2072ddd3e8aa86a6443333553841e9
Ha Phu Nguyen <hpn888@gmail.com>
2014-05-06 05:58:57 -0400

    clear hv pass

--- source_web_origin/hv/libs/connect.php
+++ source_web_origin/hv/libs/connect.php
@@ -3,7 +3,7 @@ error_reporting(0);
 $db_config_real_ip = "172.28.40.250";
 //$db_config_real_ip = "127.0.0.1";
 $db = "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = ".$db_config_real_ip.")(PORT = 1521)))(CONNECT_DATA=(SID=SDHbk)))";
-$db_conn = oci_connect("webweb", "weB210810",$db,"UTF8");
+$db_conn = oci_connect("webweb", "",$db,"UTF8");
  
 $id_title = $_GET["id"];
 ?>
