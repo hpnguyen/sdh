2f8217cc365903cdba8deb6deb21979b0af934ed
Ha Phu Nguyen <hpn888@gmail.com>
2014-05-06 05:23:04 -0400

    clear pass

--- source_web_origin/libs/connect1.php
+++ source_web_origin/libs/connect1.php
@@ -2,5 +2,5 @@
 //$db_config_real_ip = "172.28.40.250";
 $db_config_real_ip = "127.0.0.1";
 $db = "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = ".$db_config_real_ip.")(PORT = 1521)))(CONNECT_DATA=(SID=SDHbk)))";
-$db_conn_hv = OCILogon("webweb", "weB210810",$db);
+$db_conn_hv = OCILogon("webweb", "",$db);
 ?>
