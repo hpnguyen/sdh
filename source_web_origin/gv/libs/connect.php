54076026d17bf287359160bf7d98332c59fd7efb
code9000 <ngotrungtai@gmail.com>
2014-05-08 17:10:58 +0700

    xoa

--- source_web_origin/gv/libs/connect.php
+++
@@ -1,8 +0,0 @@
-﻿<?php
-error_reporting(0);
-$db = "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 172.28.40.168)(PORT = 1521)))(CONNECT_DATA=(SID=SDHbk)))";
-$db_conn = oci_connect("csdl", "LNMINH",$db,"UTF8");
-$config_name_db_link = "";
-//$config_name_db_link = "_249";
-//$config_name_db_link = "_248";
-?>
